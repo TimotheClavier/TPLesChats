c76e6348b22351314760656f55dec8763f461807
Timothe <clavier.timothe@gmail.com>
2020-03-18 15:40:53 +0100

    initial commit

---
+++ TPLesChat/Controllers/ChatController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPLesChat.Models;
+
+namespace TPLesChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public List<Chat> listeChats = Chat.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+           
+            return View(listeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+
+            var chat = listeChats.FirstOrDefault(c => c.Id == id);
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = listeChats.FirstOrDefault(c => c.Id == id);
+
+
+
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var chat = listeChats.FirstOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    listeChats.Remove(chat);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
